1d7bf66b59548633d8770182e8fc678f132830c3
kayri <kayri@DESKTOP-VTF28KC>
2024-09-14 09:02:46 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/PersonalComputer.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication1
+{
+    public class PersonalComputer
+    {
+        string Processor;
+        int Count_Of_Ram;
+        int Volume_Of_Disk;
+        string Operating_System;
+        string Graphic_Card;
+        
+    }
+
+    public class Office : PersonalComputer
+    {
+
+    }
+
+    public class Server : PersonalComputer
+    {
+
+    }
+
+    public class Laptop : PersonalComputer
+    {
+
+    }
+}
